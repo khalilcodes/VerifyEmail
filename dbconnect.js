6ea69b766a077907ebe01dc1dc4dc1485a4d94c1
khalilcodes <khalilali.j@gmail.com>
2019-10-18 20:03:56 +0530

    updated db in string

--- dbconnect.js
+++ dbconnect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://khalil:khalil@kb-jgdlm.gcp.mongodb.net/emailVerify?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+mongoose.connect('mongodb+srv://khalil:khalil@kb-jgdlm.gcp.mongodb.net/kb?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
     if (err) {
         throw err;
     } else {
